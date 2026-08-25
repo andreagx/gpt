5383e469a1ce9cc7b7c27bb39c8fecab3eede4ec
andreagx <andrea.gallazzi@live.com>
2026-08-25 10:52:13 +0200

    Fix offline cache for Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='scheda-palestra-github-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sprite.webp'];
+const CACHE='scheda-palestra-github-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})));});
