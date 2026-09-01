56a0467c1535c318537d16971e0e951fb4853ef6
andreagx <andrea.gallazzi@live.com>
2026-09-01 19:17:32 +0200

    Bump cache for editable Pilates timetable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='scheda-palestra-github-v6';
+const CACHE='scheda-palestra-github-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sprite.webp','./pilates-extra.js','./pilates-hours.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
