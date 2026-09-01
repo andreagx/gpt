fed90f060ab2ce4364dc12e0f2f019006179d9be
andreagx <andrea.gallazzi@live.com>
2026-09-01 19:10:20 +0200

    Refresh PWA cache for verified Pilates schedule

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='scheda-palestra-github-v4';
+const CACHE='scheda-palestra-github-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sprite.webp','./pilates-extra.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
